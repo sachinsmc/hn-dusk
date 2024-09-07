f2becc74f2541e094e89227d2b8288ecda4be069
Sachin Chavan <chavansachin295@gmail.com>
2024-09-07 20:16:25 +0400

    add darkmode css

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,19 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+// content.js
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+// Function to inject the dark mode CSS
+function injectDarkModeCSS() {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = browser.runtime.getURL("darkmode.css");
+    document.head.appendChild(link);
+}
+
+console.log("Content script running...", browser.runtime.getURL("darkmode.css"));
+console.log("Hostname: ", window.location.hostname);
+console.log("URL: ", window.location.href);
+
+// Inject the dark mode CSS when the page loads
+if (window.location.hostname === "news.ycombinator.com") {
+    injectDarkModeCSS();
+}
